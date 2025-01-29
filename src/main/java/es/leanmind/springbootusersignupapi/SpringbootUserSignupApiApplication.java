805bebed984fa5cfabef80fe9b799600c4958d5c
Jorge Aguiar Martin <jorge@leanmind.es>
2025-01-29 12:40:08 +0000

    project structure

---
+++ src/main/java/es/leanmind/springbootusersignupapi/SpringbootUserSignupApiApplication.java
@@ -0,0 +1,13 @@
+package es.leanmind.springbootusersignupapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUserSignupApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootUserSignupApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/leanmind/springbootusersignupapi/SpringbootUserSignupApiApplicationTests.java
@@ -0,0 +1,13 @@
+package es.leanmind.springbootusersignupapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUserSignupApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
